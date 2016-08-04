3f7c1311fb8f3205214e4e3fa6ddbc833b42d0ef
Romario <uca8@mail.ru>
2016-08-04 14:48:11 +0300

    init project

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=my_emploee',
+    'username' => 'root',
+    'password' => 'password',
+    'charset' => 'utf8',
+];
---
+++ migrations/m160804_114436_first_migration.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160804_114436_first_migration extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m160804_114436_first_migration cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
