6618fed6c6715d39bff9ec5ddf161f34cc4f0f38
Romario <uca8@mail.ru>
2016-08-08 20:52:19 +0300

    merge

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=my_emploee',
+    'dsn' => 'mysql:host=localhost;dbname=emploee',
     'username' => 'root',
     'password' => 'password',
     'charset' => 'utf8',
